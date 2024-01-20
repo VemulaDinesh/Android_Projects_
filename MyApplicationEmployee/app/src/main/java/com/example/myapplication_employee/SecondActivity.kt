4360e4bac10dddc82cd457361dba5847f4933d74
vemuladinesh25082002 <vemuladinesh25082002@gmail.com>
2024-01-20 21:27:44 +0530

    Updated Android Assignment

--- MyApplicationEmployee/app/src/main/java/com/example/myapplication_employee/SecondActivity.kt
+++ MyApplicationEmployee/app/src/main/java/com/example/myapplication_employee/SecondActivity.kt
@@ -1,35 +1,57 @@
+
 package com.example.myapplication_employee
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SecondActivity : AppCompatActivity() {
-    private lateinit var textemployeename : TextView
-    private lateinit var textemployeeid : TextView
-    private lateinit var textAge : TextView
-    private lateinit var textEmail: TextView
-    private lateinit var textAdress : TextView
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var dataSource: EmployeeDataSource
+    private lateinit var dbHelper: DBHelper
+    private lateinit var adapter: EmployeeAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        textemployeename=findViewById(R.id.tv_employeename)
-        textemployeeid=findViewById(R.id.tv_employeeid)
-        textAge=findViewById(R.id.tv_Age)
-        textEmail=findViewById(R.id.tv_Email)
-        textAdress=findViewById(R.id.tv_Adress)
-        val employeename=intent.getStringExtra("Employeename")
-        val employeeid=intent.getStringExtra("Employeeid")
-        val Age=intent.getStringExtra("Age")
-        val Email=intent.getStringExtra("Email")
-        val Adress=intent.getStringExtra("Adress")
-        textemployeename.text="Employeename: "+employeename
-        textemployeeid.text="Employeeid: "+employeeid
-        textAge.text="Age: "+Age
-        textEmail.text="Email: "+Email
-        textAdress.text="Adress: "+Adress
 
+        dataSource = EmployeeDataSource(this)
+        recyclerView = findViewById(R.id.recyclerView)
 
+        // Set up RecyclerView
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        // Retrieve all employees from the database
+        var employees = dataSource.getAllEmployees()
+        //dataSource.deleteAllData()
+        // Create and set the adapter for the RecyclerView
+         adapter = EmployeeAdapter(employees,
+            object : EmployeeAdapter.OnEditClickListener {
+                override fun onEditClick(employeeId: Long) {
+                    val intent = Intent(this@SecondActivity, MainActivity::class.java)
+                    intent.putExtra("edit_employee_id", employeeId)
+                    startActivity(intent)
+                }
+            },
+            object : EmployeeAdapter.OnDeleteClickListener {
+                override fun onDeleteClick(employeeId: Long) {
+
+                    dbHelper.removeEmployee(employeeId)
+
+                    adapter.updateData(dbHelper.getAllEmployees())
+                }
+            }
+         )
+
+        recyclerView.adapter = adapter
     }
+
+//    override fun onResume() {
+//        super.onResume()
+//        adapter.updateData(dbHelper.getAllEmployees())
+//    }
+
 }
